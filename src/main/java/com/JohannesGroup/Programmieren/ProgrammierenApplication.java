ca6755c3df98f88b7b5e8d99e9bdecd474c781e7
JohannesKerscher <Kerscherjohannes02@gmail.com>
2023-05-22 19:50:59 +0200

    initiale Struktur hinzugefügt

---
+++ src/main/java/com/JohannesGroup/Programmieren/ProgrammierenApplication.java
@@ -0,0 +1,13 @@
+package com.JohannesGroup.Programmieren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammierenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgrammierenApplication.class, args);
+	}
+
+}
